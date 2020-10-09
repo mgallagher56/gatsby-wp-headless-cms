73f8436886d2fde4f6d295f5e555697a248e7a22
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2020-10-09 15:41:16 +0100

    added helmet import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
+import { Helmet } from "react-helmet"
 // import Layout from "../components/layout"
 // import SEO from "../components/seo"
 
