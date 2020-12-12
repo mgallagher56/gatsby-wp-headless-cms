b63164a0d9118ae38ccf295f9b50df85114c1e5c
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2020-12-12 12:50:22 +0000

    added logo parade module

---
+++ src/components/modules/logpParade.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import card_eg from '../../assets/images/gatsby-astronaut.webp'
+
+
+export default class LogoParade extends Component {
+    constructor( props ) {
+        super( props );
+    }
+
+    render() {
+        return <section class={'row'}>
+            <div class={'col-12 d-flex flex-wrap justify-content-center'}>
+                <img class={'col-2'} src={card_eg} alt={'card image'} />
+                <img class={'col-2'} src={card_eg} alt={'card image'} />
+                <img class={'col-2'} src={card_eg} alt={'card image'} />
+                <img class={'col-2'} src={card_eg} alt={'card image'} />
+                <img class={'col-2'} src={card_eg} alt={'card image'} />
+                <img class={'col-2'} src={card_eg} alt={'card image'} />
+                <img class={'col-2'} src={card_eg} alt={'card image'} />
+                <img class={'col-2'} src={card_eg} alt={'card image'} />
+            </div>
+        </section>
+    }
+}
