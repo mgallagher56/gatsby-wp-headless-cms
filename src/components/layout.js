6bd36fa8cc164aecb940815801731fe8c99cd979
Marc <marc.gallagherreid11@bathspa.ac.uk>
2021-01-07 22:16:18 +0000

    fixed errors

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,8 +8,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import Nav from "./template-parts/navBar";
+import Header from "./Header"
+import Footer from "./Footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -23,27 +24,15 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+    return (
+        <>
+        <Header />
+            <div>
+                <main>{children}</main>
+                <Footer />
+            </div>
+        </>
+    )
 }
 
 Layout.propTypes = {
