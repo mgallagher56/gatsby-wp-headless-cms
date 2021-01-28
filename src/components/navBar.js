a67170b7a20d9e31d48962673f8af13462e82acc
Marc <marc.gallagherreid11@bathspa.ac.uk>
2021-01-28 23:26:32 +0000

    added hover state for mobile nav button

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -131,12 +131,14 @@ export default class NavBar extends Component {
                     case 'play':
                     default:
                         timelineArray.map((timeline) => {
-                            timeline.play();
+                            timeline.play( 0 );
                         })
                         break;
                     case 'pause':
+                        console.log('should pause here')
                         timelineArray.map((timeline) => {
-                            timeline.pause(0);
+                            console.log('and here');
+                            timeline.kill();
                         })
                         break;
                 }
@@ -149,7 +151,7 @@ export default class NavBar extends Component {
             <div ref={this.navBar}>
                 <nav className={`nav-mobile d-flex d-md-none position-absolute justify-content-center w-100 ${this.state.menuToggle ? 'open' : ''}`}>
                     <a id={'nav-toggle'} className={'collapsible-nav d-md-none d-flex flex-column justify-content-center'} onClick={this.handleClick}>
-                    <svg width="55" height="50" viewBox="0 0 100 100">
+                    <svg width="55" height="45" viewBox="0 0 100 100">
                         <path class="menu-icon line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
                         <path class="menu-icon line2" d="M 20,50 H 80" />
                         <path class="menu-icon line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
