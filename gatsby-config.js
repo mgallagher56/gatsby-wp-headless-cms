7e82525e14149a4b06e7c856a318ec01981b186e
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-02-05 20:17:16 +0000

    resolved offline issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,14 +28,6 @@ module.exports = {
         //        cache_busting_mode: 'none'
         //     }
         // },
-        {
-            resolve: 'gatsby-plugin-offline',
-            options: {
-               workboxConfig: {
-                  globPatterns: ['**/icon-path*']
-               }
-            }
-        },
         `gatsby-plugin-netlify-cache`,
         {
             resolve: `gatsby-source-filesystem`,
