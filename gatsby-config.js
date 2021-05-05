d2f3bebc383c3b13d87757b20bc1e68f50a8727c
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-05-05 15:06:15 +0100

    updated gatsby to 3.0 + plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,25 +12,42 @@ module.exports = {
         title: "MGR - Porfolio",
         titleTemplate: "%s · Dev MVP",
         description:
-          "Dev Things",
+            "Dev Portfolio",
         url: "https://mgr-dev.com", // No trailing slash allowed!
         author: "Marc Gallagher-Reid",
-      },
-      plugins: [
+    },
+    plugins: [
         'gatsby-plugin-react-helmet',
-        `gatsby-plugin-sass`,
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                cssLoaderOptions: {
+                    esModule: false,
+                    modules: {
+                        namedExport: false,
+                    },
+                },
+            },
+        },
         `gatsby-plugin-transition-link`,
         `gatsby-plugin-image`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
-        // {
-        //     resolve: 'gatsby-plugin-manifest',
-        //     options: {
-        //        icon: 'icon.svg',
-        //        cache_busting_mode: 'none'
-        //     }
-        // },
-        `gatsby-plugin-netlify-cache`,
+        "gatsby-plugin-sass",
+        "gatsby-plugin-gatsby-cloud",
+        {
+            resolve: "gatsby-plugin-google-analytics",
+            options: {
+                trackingId: "G-5L1QF3DK9J",
+            },
+        },
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/assets/images/icon.png",
+            },
+        },
         {
             resolve: `gatsby-source-filesystem`,
             options: {
@@ -39,63 +56,12 @@ module.exports = {
             },
         },
         {
-            resolve: `gatsby-transformer-sharp`,
-            options: {
-              // The option defaults to true
-              checkSupportedExtensions: false,
-            },
-        },
-        {
             resolve: `gatsby-source-wordpress`,
             options: {
-              /*
-               * The full URL of the WordPress site's GraphQL API.
-               * Example : 'https://www.example-site.com/graphql'
-               */
-               url: process.env.WPGRAPHQL_URL,
+                url: process.env.WPGRAPHQL_URL,
             },
-          },    
-        // {
-        //     resolve: `gatsby-source-wordpress-experimental`,
-        //     options: {
-                // url: process.env.WPGRAPHQL_URL,
-        //         verbose: true,
-        //         develop: {
-        //             hardCacheMediaFiles: true,
-        //         },
-        //         debug: {
-        //             graphql: {
-        //                 writeQueriesToDisk: true,
-        //             },
-        //         },
-        //         type: {
-        //             Post: {
-        //                 limit:
-        //                     process.env.NODE_ENV === `development`
-        //                         ? // Lets just pull 50 posts in development to make it easy on ourselves.
-        //                         50
-        //                         : // and we don't actually need more than 5000 in production for this particular site
-        //                         5000,
-        //             },
-        //         },
-        //         auth: {
-        //             htaccess: {
-        //                 username: process.env.HTTPBASICAUTH_USERNAME,
-        //                 password: process.env.HTTPBASICAUTH_PASSWORD,
-        //             },
-        //         },
-        //     },
-        // },
-        {
-            resolve: `gatsby-wordpress-inline-images`,
-            options:
-            {
-              baseUrl: `https://mgr-dev.online`,
-              protocol: `https`,
-              useACF: true,
-              withWebp: true,
-            }
         },
+        
         {
             resolve: "gatsby-plugin-react-svg",
             options: {
