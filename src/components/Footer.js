b6e41f6a2e1136fa2eff5b13166ff050348884ee
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-05-07 10:47:24 +0100

    lighthouse updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,7 @@ const Footer = () => (
     <footer className={'container'} style={{
         marginTop: `2rem`,
         }}>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        © {new Date().getFullYear()}
     </footer>
 )
 
