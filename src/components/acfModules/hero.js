8c80a8cbc9a12d82cd3aab97abc88ea639fb9593
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-05-07 19:48:56 +0100

    hero style updates

--- src/components/acfModules/hero.js
+++ src/components/acfModules/hero.js
@@ -8,15 +8,15 @@ export default class Hero extends Component {
         let altTag = null !== this.props.module.image ? this.props.module.image.altText : '';
 
         return <section className={'hero-module mb-5 mt-md-5'}>
-            <div className={'row d-flex flex-column flex-md-row'}>
-                <div className={'hero-media d-flex col-12 col-md-7 overflow-hidden'}>
+            <div className={'row h-100 d-flex'}>
+                <div className={'hero-media d-flex col-12 col-lg overflow-hidden'}>
                     <GatsbyImage
                         className={'hero_image'}
                         image={image}
                         alt={altTag}
                     />
                 </div>
-                <div className={'hero-content d-flex flex-column text-center justify-content-center align-items-center col-12 col-md-5 p-5'}
+                <div className={'hero-content d-flex flex-column text-center justify-content-center align-items-center col-12 p-5 col-lg mx-auto'}
                     dangerouslySetInnerHTML={{ __html: this.props.module.wysiwyg }} />
             </div>
         </section>
