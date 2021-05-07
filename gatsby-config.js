16d58f44afb714c562782a07de30a8b11d1b6248
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-05-07 19:00:44 +0100

    added font preloading

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,7 @@ module.exports = {
         `gatsby-plugin-image`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
+        `gatsby-plugin-preload-fonts`,
         {
             resolve: `gatsby-plugin-gatsby-cloud`,
             options: {
