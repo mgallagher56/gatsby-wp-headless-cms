8d9a2f85057c4d11cc08fa242ace2571d0fbe2a4
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-05-07 17:02:48 +0100

    added role to pagination dots gp

--- src/components/acfModules/cardContainer.js
+++ src/components/acfModules/cardContainer.js
@@ -1,48 +1,57 @@
 import React, { Component } from 'react';
 import GliderComponent from 'react-glider-carousel';
+import { getDocument } from 'ssr-window';
 import ModuleParser from '../ModuleParser';
-
+const document = getDocument();
 export default class CardContainer extends Component {
     render() {
         let cards = this.props.module.cards;
         let cardType = this.props.module.cardType;
 
-        if ( null !== this.props.module.cards ) {
-            if ( 'longcard' !== cardType ) {
-                return(
+        // add tab role to pagination dots in glider component
+        this.gliderDots = document.querySelectorAll('.glider-dot');
+        let len = this.gliderDots.length;
+        let i;
+        for (i = 0; i < len; i++) {
+            this.gliderDots[i].setAttribute('role', 'tab');
+        }
+
+        if (null !== this.props.module.cards) {
+            if ('longcard' !== cardType) {
+                return (
                     <section className={'container'}>
                         <div className={'row'}>
                             <GliderComponent
-                            // hasArrows={true}
-                            hasDots={true}
-                            draggable={true}
-                            // arrows={{
-                            //     prev: 'glider-prev',
-                            //     next: 'glider-next'
-                            //   }}
-                            settings={{
-                                slidesToShow: '3',
-                                slidesToScroll: 1,
-                                scrollLock: false,
-                                rewind: true,
-                                draggable: true,
-                                duration: 1
-                            }}>
-                                <ModuleParser moduleData={ cards } moduleName={ cardType } />
+                                // hasArrows={true}
+                                hasDots={true}
+                                draggable={true}
+                                // arrows={{
+                                //     prev: 'glider-prev',
+                                //     next: 'glider-next'
+                                //   }}
+                                settings={{
+                                    slidesToShow: '3',
+                                    slidesToScroll: 1,
+                                    scrollLock: false,
+                                    rewind: true,
+                                    draggable: true,
+                                    duration: 1
+                                }}>
+                                <ModuleParser moduleData={cards} moduleName={cardType} />
                             </GliderComponent>
                         </div>
                     </section>
                 )
             } else {
-                return( <section className={'container'}>
-                        <div className={'row'}>
-                            <ModuleParser moduleData={ cards } moduleName={ cardType } />
-                        </div>
+                return (<section className={'container'}>
+                    <div className={'row'}>
+                        <ModuleParser moduleData={cards} moduleName={cardType} />
+                    </div>
                 </section>
                 )
             }
         } else {
-            return( <section className={'container'}>
+            return (<section className={'container'}>
                 <h3>Add cards to card container in wordpress</h3>
             </section>
             )
