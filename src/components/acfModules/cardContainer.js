2b97636e22d92fc461c0eaf0e9a4d8ef10959cfb
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-05-09 22:31:11 +0100

    readded glider js with accessible tabs

--- src/components/acfModules/cardContainer.js
+++ src/components/acfModules/cardContainer.js
@@ -1,58 +1,47 @@
 import React, { Component } from 'react';
-import GliderComponent from 'react-glider-carousel';
-import { getDocument } from 'ssr-window';
+import Glider from '../glider';
 import ModuleParser from '../ModuleParser';
-const document = getDocument();
 export default class CardContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.glider = React.createRef();
+        this.cards = this.props.module.cards;
+        this.cardType = this.props.module.cardType;
+    }
+
+    componentDidMount() {
+        new Glider(this.glider.current, {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            draggable: true,
+            dots: '.dots',
+        })
+    }
+
     render() {
-        let cards = this.props.module.cards;
-        let cardType = this.props.module.cardType;
-
-        // add tab role to pagination dots in glider component
-        this.gliderDots = document.querySelectorAll('.glider-dot');
-        let len = this.gliderDots.length;
-        let i;
-        for (i = 0; i < len; i++) {
-            this.gliderDots[i].setAttribute('role', 'tab');
-            console.log(this.gliderDots[i].attributes);
-        }
-
-        if (null !== this.props.module.cards) {
-            if ('longcard' !== cardType) {
+        if (null !== this.cards) {
+            if ('longcard' !== this.cardType) {
                 return (
-                    <section className={'container'}>
+                    <section className={'card-container container'}>
                         <div className={'row justify-content-center'}>
-                            {/* <GliderComponent
-                                // hasArrows={true}
-                                hasDots={true}
-                                draggable={true}
-                                // arrows={{
-                                //     prev: 'glider-prev',
-                                //     next: 'glider-next'
-                                //   }}
-                                settings={{
-                                    slidesToShow: '3',
-                                    slidesToScroll: 1,
-                                    scrollLock: false,
-                                    rewind: true,
-                                    draggable: true,
-                                    duration: 1
-                                }}> */}
-                                <ModuleParser moduleData={cards} moduleName={cardType} />
-                            {/* </GliderComponent> */}
+                            <div ref={this.glider} className="glider glider-persp draggable" id="glider-persp">
+                                <ModuleParser moduleData={this.cards} moduleName={this.cardType} />
+                            </div>
+                            <div role="tablist" className="dots"></div>
                         </div>
                     </section>
                 )
             } else {
                 return (<section className={'container'}>
                     <div className={'row'}>
-                        <ModuleParser moduleData={cards} moduleName={cardType} />
+                        <ModuleParser moduleData={this.cards} moduleName={this.cardType} />
                     </div>
                 </section>
                 )
             }
         } else {
-            return (<section className={'container'}>
+            return (
+            <section className={'card-container container'}>
                 <h3>Add cards to card container in wordpress</h3>
             </section>
             )
