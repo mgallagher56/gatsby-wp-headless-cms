6aebd80e0aa60ee1b94b5507fd2089fc37dd6cbd
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-05-17 12:08:38 +0100

    test remove custom gatsby caching

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
             options: {
                 allPageHeaders: [
                     `Strict-Transport-Security: max-age=31536000; includeSubDomains; preload`,
-                    `Cache-Control: max-age=31536000`,
+                    // `Cache-Control: max-age=31536000`,
                     `Permissions-Policy: geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(self), vibrate=(), fullscreen=(self), payment=()`
                 ],
             }
