de075477d29ffc7f288013bee2c141e68859affd
Marc Gallagher-Reid <marc.gr@velocitypartners.com>
2021-06-23 11:20:33 +0100

    updated card containers, cards and hero. Removed react-bootstrap

--- src/components/cards/heroCard.js
+++ src/components/cards/heroCard.js
@@ -15,15 +15,15 @@ export default class HeroCard extends Component {
         let altTag = null !== this.props.module.image ? this.props.module.image.altText : '';
 
         return <>
-            <div className={'card hero-card my-3 mx-3 col-8 col-md-4 col-lg-3'}>
+            <div className={'card hero-card my-3 mx-3 col-8 col-md-4'}>
                 <div className={'card-media'}>
                     <GatsbyImage image={image} alt={altTag} className={'h-100 w-100'} />
-                </div> 
+                </div>
                 <div className={'card-content p-2'}>
                     <a href={this.props.module.url}>
                         <h3>{this.props.module.title}</h3>
                         <p className={'p-large'}>{this.props.module.subtitle}</p>
-                        <div dangerouslySetInnerHTML={{__html: this.props.module.content }}></div>
+                        <div className='mb-3' dangerouslySetInnerHTML={{__html: this.props.module.content }}></div>
                         <Button type={'small'} href={this.props.module.url} text={this.props.module.buttonText} />
                     </a>
                 </div>
